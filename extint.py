1fbc2fabd6939ae378ee18657b76a904580a370f
Eric <erichmatt@gmail.com>
2019-01-12 10:50:16 -0500

    made a simple program to log time intervals using interupts

--- extint.py
+++ extint.py
@@ -1,4 +1,18 @@
+# main.py -- put your code here!
 import pyb
-from pyb import Pin, ExtInt
-callback = lambda e: print("stop interupting me")
-extint = pyb.ExtInt(Pin('Y1'), ExtInt.IRQ_RISING, Pin.PULL_UP, callback)
+from pyb import Pin, ExtInt, Switch
+import time
+
+start = time.ticks_ms()
+
+def log(line):
+    data = time.ticks_diff(time.ticks_ms(), start)
+    start = time.ticks_ms()
+    return print(data)    
+    
+#log = lambda e: print(time.ticks_diff(time.ticks_ms(), start))
+
+sw.callback(log)
+
+start = time.ticks_ms()
+extint = pyb.ExtInt(Pin('Y1'), ExtInt.IRQ_RISING, Pin.PULL_UP, log)
